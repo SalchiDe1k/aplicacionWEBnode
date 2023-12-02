0d33d2d676e338cba6b81ba452c303e82cd37efb
Salchi <136645301+SalchiDe1k@users.noreply.github.com>
2023-12-01 19:15:17 -0500

    inventory.js

--- db.js
+++ db.js
@@ -1,16 +1,231 @@
-// db.js
-const mysql = require('mysql');
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-12-2023 a las 01:14:32
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.2.0
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'proyectoFinal',
-});
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Conexión exitosa a la base de datos');
-});
 
-module.exports = db;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `inventory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Admin', 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `currentstock`
+--
+
+CREATE TABLE `currentstock` (
+  `productcode` varchar(45) NOT NULL,
+  `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `currentstock`
+--
+
+INSERT INTO `currentstock` (`productcode`, `quantity`) VALUES
+('1', 25);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `customers`
+--
+
+CREATE TABLE `customers` (
+  `cid` int(11) NOT NULL,
+  `customercode` varchar(45) DEFAULT NULL,
+  `fullname` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `customers`
+--
+
+INSERT INTO `customers` (`cid`, `customercode`, `fullname`, `location`, `phone`) VALUES
+(308, '1', 'jose', 'belen', '30238373');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `pid` int(11) NOT NULL,
+  `productcode` varchar(45) DEFAULT NULL,
+  `productname` varchar(45) DEFAULT NULL,
+  `costprice` double DEFAULT NULL,
+  `sellprice` double DEFAULT NULL,
+  `brand` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`pid`, `productcode`, `productname`, `costprice`, `sellprice`, `brand`) VALUES
+(136, NULL, 'pan', 12, 32, 'catipan'),
+(137, '1', 'azucar', 34000, 45000, 'Dummy Brand'),
+(138, NULL, 'azucar', 10000, 15000, 'abelw');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `purchaseinfo`
+--
+
+CREATE TABLE `purchaseinfo` (
+  `purchaseID` int(11) NOT NULL,
+  `suppliercode` varchar(45) DEFAULT NULL,
+  `productcode` varchar(45) DEFAULT NULL,
+  `date` varchar(45) DEFAULT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `totalcost` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `purchaseinfo`
+--
+
+INSERT INTO `purchaseinfo` (`purchaseID`, `suppliercode`, `productcode`, `date`, `quantity`, `totalcost`) VALUES
+(1013, '12', '1', '02/01/2020', 10, 350000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `phone` varchar(10) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(200) DEFAULT NULL,
+  `usertype` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `location`, `phone`, `username`, `password`, `usertype`) VALUES
+(20, 'Admin', 'Local', '9876543210', 'root', 'root', 'ADMINISTRATOR');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indices de la tabla `currentstock`
+--
+ALTER TABLE `currentstock`
+  ADD PRIMARY KEY (`productcode`);
+
+--
+-- Indices de la tabla `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`cid`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`pid`),
+  ADD UNIQUE KEY `productcode_UNIQUE` (`productcode`);
+
+--
+-- Indices de la tabla `purchaseinfo`
+--
+ALTER TABLE `purchaseinfo`
+  ADD PRIMARY KEY (`purchaseID`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=309;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=139;
+
+--
+-- AUTO_INCREMENT de la tabla `purchaseinfo`
+--
+ALTER TABLE `purchaseinfo`
+  MODIFY `purchaseID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1014;
+
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
